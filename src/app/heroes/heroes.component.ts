fbf205815591d6660f34dbe0370699d4e6bf8afa
Rogelio Sánchez <41391166+rogeliosp@users.noreply.github.com>
2019-02-25 13:11:49 -0600

    Cambios locales iniciales.
    Se sube projecto con los cambios que se realizaron desde su fecha de comienzo 21-febrero-2019.

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+/* Esta clase obtiene los heroes para su consulta */
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+/* Esta clase permite registrar los heroes */
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
+/* Boton para añadir heroe */
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+/* Boton para eliminar heroe */
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
+}
